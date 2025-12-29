200e098061aeb67f9f62b1b8d1e7af799379cb43
Ekafebriyanti <ekaame24@gmail.com>
2025-12-29 17:20:36 +0700

    Menambahkan import untuk berbagai screen dalam aplikasi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/announcements_screen.dart';
+import 'screens/announcement_detail_screen.dart';
+import 'screens/class_dashboard_screen.dart';
+import 'screens/material_detail_screen.dart';
+import 'screens/assignment_detail_screen.dart';
+import 'screens/profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
