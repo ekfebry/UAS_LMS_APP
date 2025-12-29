0be7d2a6449f71a3dab4c315178443bab425fd6d
Ekafebriyanti <ekaame24@gmail.com>
2025-12-30 04:27:46 +0700

    memperbaiki error menggunakan kilo code

--- lib/widgets/help_bottom_sheet.dart
+++ lib/widgets/help_bottom_sheet.dart
@@ -5,13 +5,6 @@ class HelpBottomSheet extends StatelessWidget {
   const HelpBottomSheet({super.key});
 
   @override
-  State<HelpBottomSheet> createState() => _HelpBottomSheetState();
-}
-
-class _HelpBottomSheetState extends State<HelpBottomSheet> {
-  bool isIndonesian = true;
-
-  @override
   Widget build(BuildContext context) {
     return Container(
       width: 375,
@@ -46,10 +39,7 @@ class _HelpBottomSheetState extends State<HelpBottomSheet> {
           Positioned(
             top: 20,
             left: 123,
-            child: GestureDetector(
-              onTap: () => setState(() => isIndonesian = true),
-              child: Image.asset('assets/images/ID.png', width: 35, height: 40),
-            ),
+            child: Image.asset('assets/images/ID.png', width: 35, height: 40),
           ),
           // ID Text
           Positioned(
@@ -68,10 +58,7 @@ class _HelpBottomSheetState extends State<HelpBottomSheet> {
           Positioned(
             top: 20,
             left: 215,
-            child: GestureDetector(
-              onTap: () => setState(() => isIndonesian = false),
-              child: Image.asset('assets/images/EN.png', width: 35, height: 40),
-            ),
+            child: Image.asset('assets/images/EN.png', width: 35, height: 40),
           ),
           // EN Text
           Positioned(
@@ -86,27 +73,25 @@ class _HelpBottomSheetState extends State<HelpBottomSheet> {
               ),
             ),
           ),
-          // Selection indicator (rectangle 55)
+          // Selection indicator (centered)
           Positioned(
             top: 75,
-            left: isIndonesian ? 130 : 226,
+            left: 178, // Centered between the two flags
             child: Container(
               width: 20,
               height: 4,
               color: const Color(0xFF000000).withOpacity(0.5),
             ),
           ),
-          // Content Text
+          // Content Text (Indonesian version as default)
           Positioned(
             top: 90,
-            left: isIndonesian ? 6 : 5,
+            left: 6,
             child: SizedBox(
-              width: isIndonesian ? 362 : 365,
+              width: 362,
               height: 240,
               child: Text(
-                isIndonesian
-                    ? 'Akses hanya untuk Dosen dan Mahasiswa Telkom University.\n\nLogin menggunakan Akun Microsoft Office 365 \ndengan mengikuti petunjuk berikut : \n\nUsername (Akun iGracias) ditambahkan "@365.telkomuniversity.ac.id"\nPassword (Akun iGracias) pada kolom Password.\n\nKegagalan yang terjadi pada Autentikasi disebabkan oleh \nAnda belum mengubah Password Anda menjadi  "Strong Password". \nPastikan Anda telah melakukan perubahan Password di iGracias.\n\nInformasi lebih lanjut dapat menghubungi Layanan CeLOE Helpdesk di : \n\nMail : infoceloe@telkomuniversity.ac.id\nwhatsapp : +62 821-1666-3563'
-                    : 'Access restricted only for Lecturer and Student of Telkom University\n\nLogin only using your Microsoft Office 365 Account \nby following these format :\n\nUsername (iGracias Account) followed with "@365.telkomuniversity.ac.id"\nPassword (SSO / iGracias Account) on Password Field.\n\nFailure upon Authentication could be possibly you \nhave not yet change your password into "Strong Password". \nMake sure to change your Password only in iGracias.\n\nFor further Information, please contact CeLOE Service Helpdesk :\n\nmail : infoceloe@telkomuniversity.ac.id\nwhatsapp : +62 821-1666-3563',
+                'Akses hanya untuk Dosen dan Mahasiswa Telkom University.\n\nLogin menggunakan Akun Microsoft Office 365 \ndengan mengikuti petunjuk berikut : \n\nUsername (Akun iGracias) ditambahkan "@365.telkomuniversity.ac.id"\nPassword (Akun iGracias) pada kolom Password.\n\nKegagalan yang terjadi pada Autentikasi disebabkan oleh \nAnda belum mengubah Password Anda menjadi  "Strong Password". \nPastikan Anda telah melakukan perubahan Password di iGracias.\n\nInformasi lebih lanjut dapat menghubungi Layanan CeLOE Helpdesk di : \n\nMail : infoceloe@telkomuniversity.ac.id\nwhatsapp : +62 821-1666-3563',
                 style: GoogleFonts.poppins(
                   fontWeight: FontWeight.w400,
                   fontSize: 10,
