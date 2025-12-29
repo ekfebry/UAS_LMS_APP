0db18b094a721e99e09817326ececb33eb02de76
Ekafebriyanti <ekaame24@gmail.com>
2025-12-30 06:46:40 +0700

    Menambahkan logika penggantian tab pada HomeScreen melalui indeks terpilih untuk mendukung navigasi bagian dalam aplikasi

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -36,17 +36,15 @@ class LeftSlideInPageRoute<T> extends PageRouteBuilder<T> {
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        backgroundColor: const Color(0xFFB74848),
-        foregroundColor: Colors.white,
-      ),
-      body: const Center(
-        child: Text('Home Screen'),
-      ),
-    );
-  }
+ @override
+  State<HomeScreen> createState() => _HomeScreenState();
 }
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
