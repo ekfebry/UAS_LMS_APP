3cc30863353270c7e01f99a3ef5e7718f88aeeec
Ekafebriyanti <ekaame24@gmail.com>
2025-12-29 17:30:45 +0700

    Menambahkan konfigurasi named routes dan menetapkan SplashScreen sebagai initialRoute untuk mempermudah navigasi aplikasi dan false pada debugShowCheckedModeBanner

--- lib/main.dart
+++ lib/main.dart
@@ -21,25 +21,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (context) => const SplashScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/home': (context) => const HomeScreen(),
+        '/announcements': (context) => const AnnouncementsScreen(),
+        '/announcement_detail': (context) => const AnnouncementDetailScreen(),
+        '/class_dashboard': (context) => const ClassDashboardScreen(),
+        '/material_detail': (context) => const MaterialDetailScreen(),
+        '/assignment_detail': (context) => const AssignmentDetailScreen(title: 'Assignment'),
+        '/profile': (context) => const ProfileScreen(),
+      },
     );
   }
 }
