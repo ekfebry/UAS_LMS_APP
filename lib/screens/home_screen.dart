ee2108d98577d482e3a0d347480e60df257e6b65
Ekafebriyanti <ekaame24@gmail.com>
2025-12-30 06:43:07 +0700

    Menambahkan custom page route LeftSlideInPageRoute untuk animasi transisi geser dari kanan dengan durasi konsisten pada navigasi halaman

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,34 @@ import '../widgets/classes_tab.dart';
 import '../widgets/profile_tab.dart';
 import '../widgets/notifications_tab.dart';
 
+class LeftSlideInPageRoute<T> extends PageRouteBuilder<T> {
+  final Widget page;
+
+  LeftSlideInPageRoute({required this.page})
+      : super(
+          pageBuilder: (context, animation, secondaryAnimation) => page,
+          transitionDuration: const Duration(milliseconds: 300),
+          reverseTransitionDuration: const Duration(milliseconds: 300),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            const Offset begin = Offset(1.0, 0.0); // Slide in from right
+            const Offset end = Offset.zero;
+            
+            final Animation<Offset> slideAnimation = Tween<Offset>(
+              begin: begin,
+              end: end,
+            ).animate(CurvedAnimation(
+              parent: animation,
+              curve: Curves.easeOut, // animation-timing-function: ease-out
+            ));
+
+            return SlideTransition(
+              position: slideAnimation,
+              child: child,
+            );
+          },
+        );
+}
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
