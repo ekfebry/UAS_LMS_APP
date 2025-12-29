828ede8fa9903caf06894e86159f7e5f4668241e
Ekafebriyanti <ekaame24@gmail.com>
2025-12-29 16:29:27 +0700

    membuat class SplashScreen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState()
+}
