ec090cf5eb310d28f98edf3d3bc64cd090a8612c
Ekafebriyanti <ekaame24@gmail.com>
2025-12-30 19:56:22 +0700

    Menambahkan struktur awal QuizReviewScreen sebagai StatefulWidget

--- lib/screens/quiz_review_screen.dart
+++ lib/screens/quiz_review_screen.dart
@@ -3,3 +3,10 @@ import 'package:google_fonts/google_fonts.dart';
 import 'review_page_screen.dart';
 import 'quiz_page_screen.dart';
 import 'class_dashboard_screen.dart';
+
+class QuizReviewScreen extends StatefulWidget {
+  const QuizReviewScreen({super.key});
+
+  @override
+  State<QuizReviewScreen> createState() => _QuizReviewScreenState();
+}
