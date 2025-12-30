64355de7ad823ee4d63c2a679391864c69a7caee
Ekafebriyanti <ekaame24@gmail.com>
2025-12-30 09:21:30 +0700

    Menambahkan detail konten pada halaman detail Pengumuman berupa gambar user, informasi waktu publikasi, dan gambar pengumuman untuk memperkaya penyajian informasi kepada pengguna.

--- lib/screens/announcement_detail_screen.dart
+++ lib/screens/announcement_detail_screen.dart
@@ -47,3 +47,52 @@ class AnnouncementDetailScreen extends StatelessWidget {
               ),
             ),
           ),
+          // User Image
+          Positioned(
+            top: 55, // 166 - 80
+            left: 28,
+            child: Image.asset(
+              'assets/images/user.png',
+              width: 23,
+              height: 22,
+            ),
+          ),
+          // Date Text
+          Positioned(
+            top: 63, // 170 - 80
+            left: 58,
+            child: SizedBox(
+              width: 179,
+              height: 14,
+              child: Text(
+                'By Admin Celoe - Rabu, 2 Juni 2021, 10:45',
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 9,
+                  height: 1.0,
+                  letterSpacing: 0.0,
+                  color: const Color(0xBF000000), // 75% opacity black
+                  decoration: TextDecoration.none,
+                ),
+              ),
+            ),
+          ),
+          // Maintenance Image
+          Positioned(
+            top: 95, // 198 - 80
+            left: 28,
+            child: Container(
+              width: 319,
+              height: 185,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                  'assets/images/maintenance.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
