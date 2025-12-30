137e163f79bec0ff44f5b6879b3cf4df30ccf8ee
Ekafebriyanti <ekaame24@gmail.com>
2025-12-30 23:01:16 +0700

    Menambahkan komponen bottom sheet untuk mengunggah file tugas

--- lib/widgets/upload_file_bottom_sheet.dart
+++ lib/widgets/upload_file_bottom_sheet.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:file_picker/file_picker.dart';
+
+class UploadFileBottomSheet extends StatefulWidget {
+  const UploadFileBottomSheet({super.key});
+
+  @override
+  State<UploadFileBottomSheet> createState() => _UploadFileBottomSheetState();
+}
