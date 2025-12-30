76ff85f1739e6558b230cd5a4a83644a230d1c2e
Ekafebriyanti <ekaame24@gmail.com>
2025-12-30 08:33:53 +0700

    Menambahkan AppBar pada halaman Pengumuman dengan tombol kembali dan judul terpusat untuk meningkatkan navigasi serta kejelasan konteks tampilan.

--- lib/screens/announcement_screen.dart
+++ lib/screens/announcement_screen.dart
@@ -8,13 +8,20 @@ class AnnouncementsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Announcements'),
-        backgroundColor: const Color(0xFFB74848),
-        foregroundColor: Colors.white,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        toolbarHeight: 80,
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: Text(
+          'Pengumuman',
+          style: GoogleFonts.poppins(
+            fontWeight: FontWeight.w500,
+            fontSize: 18,
+            color: Colors.black,
+          ),
+        ),
       ),
-      body: const Center(
-        child: Text('Announcements Screen'),
-      ),
-    );
-  }
-}
