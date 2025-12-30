978bf551d88b73bfc8709358c09d39f6772ac8e5
Ekafebriyanti <ekaame24@gmail.com>
2025-12-30 07:08:16 +0700

    Mengimpor halaman-halaman utama seperti Home, Pengumuman, Dashboard Kelas, dan Detail Tugas untuk mendukung navigasi lintas fitur

---
+++ lib/widgets/classes_tab.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../screens/assignment_detail_screen.dart';
+import '../screens/announcements_screen.dart';
+import '../screens/class_dashboard_screen.dart';
+import '../screens/home_screen.dart'; 
