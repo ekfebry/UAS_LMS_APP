7aedb4cee2115ee4ef4c35b86b85630f2cf65244
Ekafebriyanti <ekaame24@gmail.com>
2025-12-30 21:26:39 +0700

    Membuat struktur awal untuk halaman ReviewPageScreen dengan StatefulWidget

--- lib/screens/review_page_screen.dart
+++ lib/screens/review_page_screen.dart
@@ -2,3 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'quiz_review_detail_screen.dart';
 import 'quiz_review_screen.dart';
+
+class ReviewPageScreen extends StatefulWidget {
+  const ReviewPageScreen({super.key});
+
+  @override
+  State<ReviewPageScreen> createState() => _ReviewPageScreenState();
+}
