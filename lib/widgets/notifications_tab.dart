0b72255d69c53efd188aa65545279b4ed79ab1fb
Ekafebriyanti <ekaame24@gmail.com>
2025-12-30 08:15:24 +0700

    Menambahkan struktur awal NotificationsTab dengan parameter callback untuk navigasi kembali, sebagai dasar tampilan notifikasi di dalam aplikasi.

--- lib/widgets/notifications_tab.dart
+++ lib/widgets/notifications_tab.dart
@@ -1,2 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+class NotificationsTab extends StatelessWidget {
+  final VoidCallback onBack;
+
+  const NotificationsTab({super.key, required this.onBack});
+
+}
