185ad4a1e7ccd33b66ce73982437d65157456690
Ekafebriyanti <ekaame24@gmail.com>
2025-12-30 19:35:06 +0700

    tambah QuizPage3Screen sebagai halaman lanjutan pada alur kuis

--- lib/screens/quiz_page3_screen.dart
+++ lib/screens/quiz_page3_screen.dart
@@ -3,3 +3,9 @@ import 'package:google_fonts/google_fonts.dart';
 import 'quiz_page2_screen.dart';
 import 'review_page_screen.dart';
 
+class QuizPage3Screen extends StatefulWidget {
+  const QuizPage3Screen({super.key});
+
+  @override
+  State<QuizPage3Screen> createState() => _QuizPage3ScreenState();
+}
