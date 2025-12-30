ee928fc12adf6c292e8ff2fb8f81bf1584412f55
Ekafebriyanti <ekaame24@gmail.com>
2025-12-30 08:40:28 +0700

    Menambahkan item pengumuman ketiga pada halaman Pengumuman dengan konten judul, metadata admin dan waktu, serta navigasi menuju detail untuk memberikan informasi yang lebih lengkap kepada pengguna.

--- lib/screens/announcement_screen.dart
+++ lib/screens/announcement_screen.dart
@@ -149,3 +149,66 @@ class AnnouncementsScreen extends StatelessWidget {
               ),
             ),
           ),
+          // Third Announcement
+          Positioned(
+            top: 120,
+            left: 0,
+            right: 0,
+            height: 50,
+            child: GestureDetector(
+              onTap: () => Navigator.pushNamed(context, '/announcement_detail'),
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 0,
+                    left: 21,
+                    child: Image.asset(
+                      'assets/images/pengumuman.png',
+                      width: 27,
+                      height: 26,
+                    ),
+                  ),
+                  Positioned(
+                    top: 0,
+                    left: 58,
+                    child: SizedBox(
+                      width: 296,
+                      height: 18,
+                      child: Text(
+                        'Maintenance Pra UAS Semeter Ganjil 2020/2021',
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12,
+                          height: 1.0,
+                          letterSpacing: 0.0,
+                          color: Colors.black,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 15,
+                    left: 58,
+                    child: SizedBox(
+                      width: 179,
+                      height: 14,
+                      child: Text(
+                        'By Admin Celoe - Minggu, 10 Januari 2021, 9:30',
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 9,
+                          height: 1.0,
+                          letterSpacing: 0.0,
+                          color: const Color(0xBF000000), // 75% opacity black
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
