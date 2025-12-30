66a1ef75c3d160b41094db0e4f789d6c01fa9d56
Ekafebriyanti <ekaame24@gmail.com>
2025-12-30 18:29:32 +0700

    membuat struktur awal QuizPageScreen sebagai StatefulWidget

--- lib/screens/quiz_page_screen.dart
+++ lib/screens/quiz_page_screen.dart
@@ -2,3 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'quiz_page2_screen.dart';
 
+class QuizPageScreen extends StatefulWidget {
+  const QuizPageScreen({super.key});
+
+  @override
+  State<QuizPageScreen> createState() => _QuizPageScreenState();
+}
