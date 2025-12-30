1054f2059184143caddaa4a7a235a18b3a8951dc
Ekafebriyanti <ekaame24@gmail.com>
2025-12-30 10:25:55 +0700

    Membuat struktur awal ProfileScreen sebagai StatefulWidget untuk memungkinkan pengelolaan dan pembaruan status pada tampilan profil pengguna.

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -2,20 +2,9 @@ import 'package:flutter/material.dart';
 import 'package: google_fonts/google_fonts.dart';
 import '../widgets/profile_tab.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-        backgroundColor: const Color(0xFFB74848),
-        foregroundColor: Colors.white,
-      ),
-      body: const Center(
-        child: Text('Profile Screen'),
-      ),
-    );
-  }
+   State<ProfileScreen> createState() => _ProfileScreenState();
 }
