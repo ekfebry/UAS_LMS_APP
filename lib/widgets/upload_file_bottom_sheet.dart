86ebe1515db29c8f9bf8a03f91be4310a8a7bbd6
Ekafebriyanti <ekaame24@gmail.com>
2025-12-30 23:14:17 +0700

    Menambahkan UI Upload File pada Bottom Sheet dengan batas ukuran dan jumlah file serta AppBar khusus

--- lib/widgets/upload_file_bottom_sheet.dart
+++ lib/widgets/upload_file_bottom_sheet.dart
@@ -26,3 +26,44 @@ class _UploadFileBottomSheetState extends State<UploadFileBottomSheet> {
       });
     }
   }
+
+   @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.7,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(25),
+          topRight: Radius.circular(25),
+        ),
+      ),
+      child: Column(
+        children: [
+          AppBar(
+            backgroundColor: const Color(0xFFB74848),
+            elevation: 0,
+            automaticallyImplyLeading: false,
+            centerTitle: true,
+            title: Text(
+              'Upload File',
+              style: GoogleFonts.poppins(
+                fontWeight: FontWeight.w600,
+                fontSize: 14,
+                color: Colors.white,
+              ),
+            ),
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25),
+                topRight: Radius.circular(25),
+              ),
+            ),
+          ),
+
+          const SizedBox(height: 12),
+
+          Text(
+            'Maksimum File 5MB , Maksimum Jumlah File 20',
+            style: GoogleFonts.poppins(fontSize: 12),
+          ),
