a404b798328e94b99dbc6ac3e25690a2acd7fedd
Ekafebriyanti <ekaame24@gmail.com>
2025-12-30 17:26:00 +0700

    Menambahkan konten kuis dan informasi tenggat waktu pada bagian Pertemuan 3 di ClassDashboardScreen

--- lib/screens/class_dashboard_screen.dart
+++ lib/screens/class_dashboard_screen.dart
@@ -1078,3 +1078,126 @@ class _ClassDashboardScreenState extends State<ClassDashboardScreen> {
                     ),
                   ),
                 ),
+                // Third rectangle
+                Positioned(
+                  top: 329,
+                  left: 21,
+                  child: Container(
+                    width: 332,
+                    height: 123,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 4,
+                          offset: const Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 339,
+                  left: 35,
+                  child: Container(
+                    width: 92,
+                    height: 22,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF53A9E3),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 344,
+                  left: 50,
+                  child: SizedBox(
+                    width: 62,
+                    height: 15,
+                    child: Text(
+                      'Pertemuan 3',
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 10,
+                        color: Colors.white,
+                        height: 1.0,
+                        letterSpacing: 0,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 338,
+                  left: 323,
+                  child: SizedBox(
+                    width: 22,
+                    height: 16,
+                    child: Image.asset(
+                      'assets/images/green_tick.png',
+                      width: 22,
+                      height: 16,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 388,
+                  left: 85,
+                  child: SizedBox(
+                    width: 255,
+                    height: 21,
+                    child: Text(
+                      'Kuis - Assessment 2',
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14,
+                        color: const Color(0xFF000000),
+                        height: 1.0,
+                        letterSpacing: 0,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 433,
+                  left: 35,
+                  child: SizedBox(
+                    width: 280,
+                    height: 15,
+                    child: Text(
+                      'Tenggat Waktu : 26 Februari 2021 23:59 WIB',
+                      style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 10,
+                        color: const Color(0x80000000),
+                        height: 1.0,
+                        letterSpacing: 0,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 380,
+                  left: 35,
+                  child: Opacity(
+                    opacity: 1,
+                    child: SizedBox(
+                      width: 46,
+                      height: 37,
+                      child: Image.asset(
+                        'assets/images/quiz.png',
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      default:
+        return Container();
+    }
+  }
