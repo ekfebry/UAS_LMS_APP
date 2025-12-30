1da1e01c04b1805360fe437e805ed4036aa2c0ac
Ekafebriyanti <ekaame24@gmail.com>
2025-12-30 20:56:39 +0700

    Menambahkan struktur awal halaman QuizReviewDetailScreen

--- lib/screens/quiz_review_detail_screen.dart
+++ lib/screens/quiz_review_detail_screen.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'review_page_screen.dart';
+
+class QuizReviewDetailScreen extends StatefulWidget {
+  const QuizReviewDetailScreen({super.key});
+
+  @override
+  State<QuizReviewDetailScreen> createState() => _QuizReviewDetailScreenState();
+}
